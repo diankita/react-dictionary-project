00f79d3b192aeca96a8781aded9a7029ce1f6085
dianasul <sulkova.diana@gmail.com>
2023-01-16 22:29:36 +0100

    Added a footer with Github link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div className="App">
       <header className="App-header"></header>
       <Search />
+      <footer>
+        Coded by Diankita and{" "}
+        <a href="https://github.com/diankita/react-dictionary-project">
+          open sourced
+        </a>
+      </footer>
     </div>
   );
 }
